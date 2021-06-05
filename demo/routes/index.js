7016edcc1966dffd4ef3f1f892f6f2d0710f1789
weima12345-abc <18174001013@139.com>
2021-06-06 01:43:04 +0800

    数据库改进

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -11,9 +11,8 @@ let connection=mysql.createConnection({
 router.get('/',(req,res)=>{
     
       
-         connection.query("select * from book a INNER JOIN 租赁表 b on a.`名称`=b.`书籍名称` limit 0,6; ",function(err,a,fieids){  
-                console.log(a);
-                
+         connection.query(" select * from book",function(err,a,fieids){  
+           
             res.render('index',{detail:a});    
          });
        
