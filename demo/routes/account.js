b895a4f3d7ee877dab00a43b19b7c43a291a9ace
weima12345-abc <18174001013@139.com>
2021-06-08 00:37:55 +0800

    改进关于只有已经注册了的用户才能登录的bug

--- demo/routes/account.js
+++ demo/routes/account.js
@@ -17,12 +17,23 @@ router.get('/', (req, res)=> {
 
  //Already 注册
 router.post('/',(req,res)=>{ 
-  let sql=`select *from user  where password="${req.body.password}" and email="${req.body.email}"`;
-           connection.query(sql,function(err,b,fields){
-                  if(req.body.password==b[0].password&&req.body.email==b[0].email){
-                      res.redirect('g');    
-                } else{
-                  res.render('indent_fail');  
-                }
-                   }); })                   
+
+           connection.query("select *from user  where password=? and email=?",[req.body.password,req.body.email],function(err,b,fields){
+            if(b[0]!=undefined){
+              res.redirect('g');
+            }else{
+              res.render('indent_fail');                    
+            }
+
+                //   if(req.body.password==b[0]&&req.body.email=="1@1.com"){
+                //       res.redirect('g');    
+                // }
+                // else{
+                //   res.render('indent_fail');                    
+                // }
+
+          
+
+                   });
+                   })                   
 module.exports = router;
